1370114cca1dcfcb6c2c41db2224fc6672c60136
codoncareai-oss <codoncareai@gmail.com>
2026-02-09 21:01:50 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -109,6 +109,6 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
